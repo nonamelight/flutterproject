aa2c138fa0ea10fd7dd53af0e7b6e39fb408c69f
nonamelight <nonamelight1@naver.com>
2021-10-09 17:14:25 +0900

    2

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyApp();
+  }
+}
+
+class _MyApp extends State<MyApp> {
+  // This widget is the root of your application.
+  var switchValue = false;
+  String test = 'hello';
+  Color _color = Colors.blue;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        darkTheme: ThemeData.light(),
+        home: Scaffold(
+          body: Center(
+            child: ElevatedButton(
+                child: Text('$test'),
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(_color)),
+                onPressed: () {
+                  if (test == 'hello') {
+                    setState(() {
+                      test = 'flutter';
+                      _color = Colors.amber;
+                    });
+                  } else {
+                    setState(() {
+                      test = 'hello';
+                      _color = Colors.blue;
+                    });
+                  }
+                }),
+          ),
+        ));
+  }
+}
