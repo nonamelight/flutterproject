0180a8e0a3ff4a1de90b5bd33035c08eaa00e81b
nonamelight <nonamelight1@naver.com>
2021-10-09 18:10:36 +0900

    1.flutter 생명주기 console 출력

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ void main() {
 class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
+    print('createState');
     return _MyApp();
   }
 }
@@ -19,6 +20,7 @@ class _MyApp extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
+    print('build');
     return MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
@@ -47,5 +49,16 @@ class _MyApp extends State<MyApp> {
                 }),
           ),
         ));
+  }
+  @override
+  void initState(){
+    super.initState();
+    print('initState');
+  }
+
+  @override
+  void didChangeDependencies(){
+    super.didChangeDependencies();
+    print('didChangeDependencies');
   }
 }
